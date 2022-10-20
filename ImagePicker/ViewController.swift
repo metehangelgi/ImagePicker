b6b0c9ac224dea307f9841c0735c79fe2c6f9576
Metehan Gelgi <mgelgi17@ku.edu.tr>
2022-10-20 16:13:30 +0300

    Initial commit

---
+++ ImagePicker/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Gun Makinabakan on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func selectImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        picker.allowsEditing = true
+        self.present(picker, animated: true)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        self.imageView.image = image
+        self.dismiss(animated: true)
+    }
+}
